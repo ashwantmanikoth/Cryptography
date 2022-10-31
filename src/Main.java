962b346df788478d7fc6e34681d57a5377e3ac76
ashwantmanikoth <ashwanta75@gmail.com>
2022-10-31 10:22:30 -0300

    first initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        do {
+            System.out.println("Enter your choice \n 1-Encrypt\n 2-Decrypt\n");
+            Scanner scanner = new Scanner(System.in);
+            a = scanner.nextInt();
+            if (a == 1) {
+                System.out.println("\nEnter the message which needs to be encrypted\n");
+                BigInteger value;
+                value = scanner.nextBigInteger();
+                KeyGen keyGen = new KeyGen();
+                System.out.println("e  is ->" + keyGen.e);
+                BigInteger cipherText = keyGen.encrypt(value);
+                System.out.println("\nThe Encrypted cipher is \n" + cipherText);
+            } else if (a == 2) {
+                System.out.println("\nEnter the cipher text which needs to be decrypted\n");
+                BigInteger value;
+                value = scanner.nextBigInteger();
+                KeyGen keyGen = new KeyGen();
+                System.out.println("e  is ->" + keyGen.e);
+                BigInteger decryptedMessage = keyGen.decrypt(value);
+                System.out.println("\nThe decrypted message is\n" + decryptedMessage);
+            }
+        } while (a == 3);
+    }
+}
+
